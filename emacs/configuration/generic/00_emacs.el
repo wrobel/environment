0baadd709a8eaeb323d5495211761738fa8fbbe9
Gunnar Wrobel <p@rdus.de>
2009-11-29 18:03:28 +0100

    Additional emacs configuration.

--- emacs/configuration/generic/00_emacs.el
+++ emacs/configuration/generic/00_emacs.el
@@ -31,10 +31,10 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;(autoload 'php-mode "01_php_mode" "PHP editing mode." t)
+(autoload 'php-mode "01_php_mode" "PHP editing mode." t)
 
-;;(timed-load "02_emacs")
+(timed-load "02_emacs")
 
 (timed-load "97_env")
 
-;;(timed-load "99_keys")
+(timed-load "99_keys")
