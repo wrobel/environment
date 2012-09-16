763a780ed2bba7dcd93e5ad5ec1bd0aa74d2d00d
Gunnar Wrobel <p@rdus.de>
2012-09-16 23:19:24 +0200

    Add PHP and git autostarts.

--- emacs/configuration/generic/00_emacs.el
+++ emacs/configuration/generic/00_emacs.el
@@ -32,6 +32,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (autoload 'php-mode "01_php_mode" "PHP editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.php[s34]?\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
+
+(autoload 'git-status "git" "Entry point into git-status mode." t)
+(autoload 'git-blame-mode "git-blame"
+  "Minor mode for incremental blame for Git." t)
+(unless (fboundp 'vc-git-registered) 
+  (add-to-list 'vc-handled-backends 'GIT))
 
 (timed-load "02_emacs")
 
