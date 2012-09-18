8213f97ca6d62cce4c86e436de490f1049938b09
Gunnar Wrobel <p@rdus.de>
2012-09-18 16:47:18 +0200

    Add escreen.

---
+++ emacs/configuration/generic/07_escreen.el
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; ESCREEN
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'escreen)
+(escreen-install)
+(setq escreen-prefix-char "\C-z")
+(global-set-key escreen-prefix-char 'escreen-prefix)
